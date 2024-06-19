04c690f63c96d934e4f1e8bbd12f52a26af2d52d
yeon <rladusdn56@gmail.com>
2024-06-19 10:11:11 +0900

    index files edited

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+let phoneNumber = '';
+
+function addToPhoneNumber(number) {
+    phoneNumber += number;
+    updatePhoneNumberDisplay();
+}
+
+function updatePhoneNumberDisplay() {
+    const phoneNumberDisplay = document.getElementById('phone-number');
+    phoneNumberDisplay.textContent = phoneNumber;
+}
+
 document.getElementById('add-number').addEventListener('click', function() {
     alert('Add Number button clicked!');
 });
-

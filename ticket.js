7271187a853544d1900fdf05dafa8823d24bf7d1
yeon <rladusdn56@gmail.com>
2024-06-20 15:47:02 +0900

    firebase connect

---
+++ ticket.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCNRlveUhSNmaWgXgvNrUgJcZOu5dQnlVU",
+    authDomain: "callg-48fb8.firebaseapp.com",
+    projectId: "callg-48fb8",
+    storageBucket: "callg-48fb8.appspot.com",
+    messagingSenderId: "868470135005",
+    appId: "1:868470135005:web:66a98742681a095e55a334"
+  };
+
+  const app = initializeApp(firebaseConfig);
